2d99d16978722d71059ce875d1fe76c8af352bad
lzaragosa555459-eng <l.zaragosa.555459@umindanao.edu.ph>
2026-03-02 15:13:49 +0800

    first commit

--- edit_student.php
+++ edit_student.php
@@ -32,6 +32,7 @@ if (isset($_POST['add'])) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit</title>
