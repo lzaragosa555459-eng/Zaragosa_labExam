4355ee83a80ab31df01d797223eead77fab7527f
lzaragosa555459-eng <l.zaragosa.555459@umindanao.edu.ph>
2026-03-02 14:56:53 +0800

    first commit

--- home.php
+++ home.php
@@ -22,24 +22,64 @@ if (isset($_POST['add'])) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
 </head>
+<style>
+    .container{
+        margin-bottom: 10px;
+        padding: 10px;
+        width: 700px;
+        padding-left: 20px;
+    }
+    
+    
+</style>
 <body>
     <h1>Student Records</h1>
-    <a href="create_student.php">Add Student+</a>
+    <a href="create_student.php" class="a" style="
+     display: inline-block;
+    background-color: #78cf49; /* Blue */
+    color: white;
+    padding: 6px 12px;
+    text-decoration: none;
+    border-radius: 5px;
+    font-size: 14px;
+    margin: 2px;
+    margin-left:47%;
+    transition: background-color 0.3s;">Add Student+</a>
 
-<h2>List of Students:</h2>
-<?php
+
+<div class="container">
+    <?php
 
 foreach ($_SESSION['students'] as $index => $s) {
     echo $s['id'] . " | " .
          $s['name'] . " | " .
          $s['email'] . " | " .
          $s['course'] . " | " .
-         "<a href='edit_student.php?index=$index'>Edit</a> | " .
-         "<a href='home.php?delete=$index' onclick=\"return confirm('Delete this student?')\">Delete</a><br>";
+         "<a href='edit_student.php?index=$index' style='
+        display: inline-block;
+        background-color: #008CBA; /* Blue */
+        color: white;
+        padding: 6px 12px;
+        text-decoration: none;
+        border-radius: 5px;
+        font-size: 14px;
+        margin: 2px;
+        transition: background-color 0.3s;'>Edit</a> | " .
+         "<a href='home.php?delete=$index' onclick=\"return confirm('Delete this student?')\" style='
+        display: inline-block;
+        background-color: #d44b47; /* Blue */
+        color: white;
+        padding: 6px 12px;
+        text-decoration: none;
+        border-radius: 5px;
+        font-size: 14px;
+        margin: 2px;
+        transition: background-color 0.3s;'>Delete</a><br>";
 }
 
 
@@ -50,6 +90,8 @@ if (isset($_GET['delete'])) {
         $_SESSION['students'] = array_values($_SESSION['students']);
     }
 }
-?>   
+?> 
+</div>
+  
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,18 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<style>
+    body{
+        background-color: white;
+    }
+    .container{
+        padding: 100px;
+        width: 450px;
+        border: 1px solid;
+    }
+    input{
+        width: 400px;
+        height: 30px;
+        border-radius: 8px;
+        border: 1px solid;
+    }
+    button{
+        margin-top: 10px;
+    }
+</style>
 <body>
     <h1>Welcome to Student Managememt System</h1>
     <h3>Enter your credentials to login!</h3>
-    <form >
-        Username: <input type="text" required>
-        Password: <input type="password" required>
-        <button type="submit"><a href="home.php">Login</a></button>
+    <div class="container">
+        <form action="home.php">
+        Username: <br><input type="text" required>
+        Password: <br><input type="password" required>
+        <button type="submit">Login</button>
     </form>
+    </div>
+    
    
 </body>
 </html>
